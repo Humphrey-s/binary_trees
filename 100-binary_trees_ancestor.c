6c1b930d3dbe531d58a5c60d667b0eb73fcbd948
greybeard <greybeard@gmail.com>
2024-03-14 15:54:07 +0300

    3 commit

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -13,8 +13,8 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
 	if ((first == NULL) || (second == NULL))
 		return (NULL);
 
-	if (ptr == second)
-		return (ptr);
+	if (first == second)
+		return (first);
 
 	while (ptr != NULL)
 	{
