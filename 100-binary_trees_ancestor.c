ece5ef040b3b53b64e0b9039d05e708f6d027ab6
greybeard <greybeard@gmail.com>
2024-03-14 17:48:45 +0300

    ccc

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -2,63 +2,50 @@
 #include "binary_trees.h"
 /**
  * binary_trees_ancestor - finds LCA node of n1 and n2
- * @n1: node 1
- * @n2: node 2
+ * @f: node 1
+ * @s: node 2
  * Return: return pointer of LCA
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *f, const binary_tree_t *s)
 {
-	binary_tree_t *sparent;
-	binary_tree_t *fparent;
-	__attribute__((unused))binary_tree_t *ptr;
+	binary_tree_t *sparent, *fparent, *ptr;
 
-	if (!first || !second)
+	if (!f || !s)
 		return (NULL);
+	sparent = s->parent;
+	fparent = f->parent;
 
-	sparent = second->parent;
-	fparent = first->parent;
-
-	if (first == second)
+	if (f == s)
 	{
-		if (sparent->right == second)
+		if (sparent->right == s)
 			return (sparent->right);
-		if (sparent->left == second)
+		if (sparent->left == s)
 			return (sparent->left);
 	}
-
 	while (fparent != NULL && sparent != NULL)
 	{
 		if (fparent == sparent)
 			return (fparent);
-
 		ptr = fparent;
 		while (ptr != NULL)
 		{
-			if (ptr == second)
+			if (ptr == s)
 				return (ptr);
-
 			if (ptr == sparent)
 				return (sparent);
-
 			ptr = ptr->parent;
 		}
-
 		ptr = sparent;
-
 		while (ptr != NULL)
 		{
-			if (ptr == first)
-                                return (ptr);
-
-                        if (ptr == fparent)
-                                return (fparent);
-
-                        ptr = ptr->parent;
+			if (ptr == f)
+				return (ptr);
+			if (ptr == fparent)
+				return (fparent);
+			ptr = ptr->parent;
 		}
-
 		fparent = fparent->parent;
 		sparent = sparent->parent;
 	}
-
 return (NULL);
 }
